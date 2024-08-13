40d7b2ef58c7116a4100b3b066e93d1120e3868a
abhay <ap391929@gmail.com>
2024-08-13 23:39:59 +0530

    feat:delete category by id

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -20,49 +20,35 @@ end
 
 end
 
-
-
 # GET /categories/1 or /categories/1.json
+
+
 
 
 # GET /categories/1/edit
 
 
-# POST /categories or /categories.json
 
 
 def show
 end
 
 def new 
-
 @category = Category.new
-
 end
 
+# POST /categories or /categories.json
 
 def create(category_params)
-
 @category = Category.new(category_params)
-
 respond_to do |format| 
-
-
   if @category.save
-
      format.json {render :show}
      puts "Category saved"
-  
   else
-     
     puts "Failed to save the category"
-  
   end
-
-  
 end
-
-
 end
 
 
@@ -71,6 +57,29 @@ end
 
 
 # DELETE /categories/1 or /categories/1.json
+
+
+def delete
+
+@category = Category.find(params[:id])
+
+
+respond_to do |format|
+
+if @category.destroy!
+format.html {notice: "Post successfully deleted"}
+format.json {head :no_content}
+
+else
+format.html {notice: "Unable to delete the post"}
+end
+
+end
+
+end
+
+
+
 
 
 # Only allow a list of trusted parameters through.
